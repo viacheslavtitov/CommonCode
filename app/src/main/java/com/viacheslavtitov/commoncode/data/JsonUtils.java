29c09e9ccaa9cc1f9e882ba7dd4928afd0d64d73
Viacheslav Titov <viacheslavtitov90@gmail.com>
2017-09-21 19:25:52 +0300

    add helper for json data

---
+++ app/src/main/java/com/viacheslavtitov/commoncode/data/JsonUtils.java
@@ -0,0 +1,24 @@
+package com.viacheslavtitov.commoncode.data;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by Viacheslav Titov on 21.09.2017.
+ */
+
+public class JsonUtils {
+
+    public static <T> T parseJsonObject(String json, Class<T> clsz) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json, clsz);
+    }
+
+    public static <T> T parseJsonList(String json, Class<T> clsz) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json, mapper.getTypeFactory().constructCollectionType(List.class, clsz));
+    }
+
+}
